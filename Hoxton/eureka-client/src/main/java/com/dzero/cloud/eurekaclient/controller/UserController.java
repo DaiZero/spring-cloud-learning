795d8b2b97f67951274cd27053db802dfe8c0d3a
DZero <dzd624175217@gmail.com>
2021-02-22 17:09:30 +0800

    添加项目

---
+++ Hoxton/eureka-client/src/main/java/com/dzero/cloud/eurekaclient/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.dzero.cloud.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/list")
+    public Map<String, Object> getUserList() {
+        System.out.println("/api/user/list 接口被调用，服务端口号：" + port);
+        Person person = new Person();
+        person.setUserId(0);
+        person.setUserName("dzero");
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("code", "200");
+        resultMap.put("success", true);
+        resultMap.put("data", person);
+        return resultMap;
+    }
+
+    class Person {
+        private Integer userId;
+        private String userName;
+
+        public Integer getUserId() {
+            return userId;
+        }
+
+        public void setUserId(Integer userId) {
+            this.userId = userId;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+    }
+
+}
