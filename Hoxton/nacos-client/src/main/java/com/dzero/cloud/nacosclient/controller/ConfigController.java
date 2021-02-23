3cc974b1363dad6035d18024442789f2672e9476
DZero <dzd624175217@gmail.com>
2021-02-23 10:12:37 +0800

    删除多余项目

---
+++ Hoxton/nacos-client/src/main/java/com/dzero/cloud/nacosclient/controller/ConfigController.java
@@ -0,0 +1,21 @@
+package com.dzero.cloud.nacosclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope    // 通过 Spring Cloud 原生注解 @RefreshScope 实现配置自动更新
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Value("${config.info}")
+    private String info;
+
+    @GetMapping("/info")
+    public Object getInfo() {
+        return "获取的配置信息为：" + info;
+    }
+}
